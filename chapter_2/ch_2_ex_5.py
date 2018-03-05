61ef2dbb4a4ada204ec4ca21921bed8f7765eab5
Chris Wong <cwong168@gmail.com>
2018-03-04 22:13:26 -0800

    First commit

---
+++ chapter_2/ch_2_ex_5.py
@@ -0,0 +1,7 @@
+#Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+Celsius = input('Celsius Temperature:')
+Fahrenheit = float(Celsius) * 9/5  + 32
+print('Fahrenheit Temperature:', Fahrenheit)
+
+
