149664c480256a6d2ce2b3c6c82583afb722421d
cwong2 <cwong0@walmartlabs.com>
2018-04-09 22:42:03 -0700

    Editing file.

--- chapter_4/ch_4_ex_7_score.py
+++ chapter_4/ch_4_ex_7_score.py
@@ -1,17 +1,21 @@
-def computegrade():
+def computergrade(hokey) :
     try:
-        score = float(input('Enter Score:'))
-        if float(score) > float(1.0) :
-            print("Error, Bad Score")
-        elif float(score) >= float(0.9) :
-            print("A")
-        elif float(score) >= float(0.8) :
-            print("B")
-        elif float(score) >= float(0.7) :
-            print("C")
-        elif float(score) >= float(0.6) :
-            print("D")
-        elif float(score) < float(0.6) :
-            print("F")
-    except ValueError:
-        print("Bad Score")
+        float_score = float(score)
+    except:
+        print("Error, Bad Score") 
+        quit()
+    if float(score) > float(1.0) :
+        print("Error, Bad Score")
+    elif float(score) >= float(0.9) :
+        print("A")
+    elif float(score) >= float(0.8) :
+        print("B")
+    elif float(score) >= float(0.7) :
+        print("C")
+    elif float(score) >= float(0.6) :
+        print("D")
+    elif float(score) < float(0.6) :
+        print("F")
+
+score = input ('Enter Score:')
+computergrade(score)
